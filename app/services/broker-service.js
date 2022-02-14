bb2ae4c816018600a5fad245599baa9a1eda8534
MarwaAbr <79973721+MarwaAbr@users.noreply.github.com>
2022-02-14 12:31:57 +0100

    Update broker-service.js

--- app/services/broker-service.js
+++ app/services/broker-service.js
@@ -6,9 +6,9 @@ import 'rxjs/Rx';
  Prettify objects returned from Salesforce. This is optional, but it allows us to keep the templates independent
  from the Salesforce specific naming convention. This could also be done Salesforce-side by creating a custom REST service.
  */
-let prettifyContact = (Contact) => {
+let prettifyContact = (contact) => {
     return {
-        id: Contact.ClientID__c,
+        id: contact.ClientID__c,
         name: contact.Name,
        // title: contact.title,
         //picture: broker.picture__c,
