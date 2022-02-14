c3b22306d1b0128d9b43b13bf5979066acabf871
MarwaAbr <79973721+MarwaAbr@users.noreply.github.com>
2022-02-14 11:40:48 +0100

    brocker to contact

--- app/services/broker-service.js
+++ app/services/broker-service.js
@@ -6,15 +6,15 @@ import 'rxjs/Rx';
  Prettify objects returned from Salesforce. This is optional, but it allows us to keep the templates independent
  from the Salesforce specific naming convention. This could also be done Salesforce-side by creating a custom REST service.
  */
-let prettifyBroker = (broker) => {
+let prettifyBroker = (contact) => {
     return {
-        id: broker.sfid,
-        name: broker.name,
-        title: broker.title__c,
-        picture: broker.picture__c,
-        phone: broker.phone__c,
-        mobilePhone: broker.mobile_phone__c,
-        email: broker.email__c
+        id: contact.sfid,
+        name: contact.name,
+        title: contact.title,
+        //picture: broker.picture__c,
+        phone: contact.phone,
+        mobilePhone: contact.mobile_phone,
+        email: contact.email
     };
 };
 
@@ -30,11 +30,11 @@ export class BrokerService {
     }
 
     findAll() {
-        return this.http.get('/broker').map(response => response.json().map(prettifyBroker));
+        return this.http.get('/contact').map(response => response.json().map(prettifyBroker));
     }
 
     findById(id) {
-        return this.http.get('/broker/' + id).map(response => prettifyBroker(response.json()));
+        return this.http.get('/contact/' + id).map(response => prettifyBroker(response.json()));
     }
 
 }
